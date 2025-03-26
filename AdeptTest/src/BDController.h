c4eb31c4ad3222f5981120cf2826eb65bd9a7a91
Jaldima17 <dimit2806@gmail.com>
2025-03-26 13:33:08 +0700

    Update BDController.h

--- AdeptTest/src/BDController.h
+++ AdeptTest/src/BDController.h
@@ -35,4 +35,20 @@ private:
 	Firebird::IXpbBuilder* _dpb;
 
 	Firebird::IAttachment* _att;
+
+	static const char* _sqlInsert;
+	static const char* _sqlGroupByDistanse;
+	static const char* _sqlGroupByName;
+	static const char* _sqlGroupByType;
+	static const char* _sqlGroupByTime;
+
+	enum Group
+	{
+		Distanse,
+		Name,
+		Type,
+		Time
+	};
+
+	std::map<std::string, std::vector<Objects>> groupBy(Group group);
 };
